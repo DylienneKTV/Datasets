aecbd421877a92bd2bf32239c764c67f29fc5c05
NathalieG123 <nathalie.germazion@hotmail.com>
2017-07-13 15:35:48 +0200

    end

---
+++ births.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+
+# US births 2000-2014
+# Dataset
+#f = pd.csv('US_births_2000-2014.csv', sep=',')
+
+# for i in f:
+#     blabla
+#     print(i)
+#
+# result = "the average of births in usa is %d in 2015"
+# print(" result ") %average of 2015
+
+
+#births_data = open('US_births_2000-2014.csv', 'r')
+#print(births_data.())
+
+
+# Feature engineering
+
+year = ['US_births_2000-2014_SSA.csv', 'year']
+births = ['US_births_2000-2014_SSA.csv', 'births']
+months =  ['US_births_2000-2014_SSA.csv', 'month'
+day_month = ['US_births_2000-2014_SSA.csv', 'date_of_month']
+day_week = ['US_births_2000-2014_SSA.csv', 'day_of_week']
+
+# Programme
+
+births_2000 = []
+for year in US_births_2000-2014.csv[0:368]:
+    year in US_births_2000-2014.csv
+    births_2000.append(year)
+print(births_2000)
--- dataset.py
+++ dataset.py
@@ -3,18 +3,18 @@ import pandas as pd
 import os
 import matplotlib.pyplot as plt
 #
-# # dataset
-# setje = pd.read_csv('/Users/dylienneevery/Downloads/Pokemon.csv', sep=',')
-#
-#
-# # feature engineering
-#
+ # dataset
+ #setje = pd.read_csv('/Users/dylienneevery/Downloads/Pokemon.csv', sep=',')
+
+
+# feature engineering
+
 # generation = setje['Generation']
 # names = setje['Name']
 # total = setje['Total']
 # atk = setje['Attack']
 # dfs = setje['Defense']
-#
+
 # # programme
 #
 # plt.xlabel('total')
@@ -41,46 +41,3 @@ import matplotlib.pyplot as plt
 # print(ratio.head())
 # print('______')
 # print(mean_ratio)
-
-
-# US births 2000-2014
-
-# f = pd.read_csv('US_births_2000-2014_SSA.csv', sep=',')
-
-# for i in f:
-#     blabla
-#     print(i)
-#
-# result = "the average of births in usa is %d in 2015"
-# print(" result ") %average of 2015
-
-
-f = open('US_births_2000-2014.csv', 'r')
-file = f.read
-print(f)
-
-# year = ['US_births_2000-2014_SSA.csv', 'year']
-# births = ['US_births_2000-2014_SSA.csv', 'births']
-# mean = mean("births")
-# if year is "2000":
-#     print(mean)
-
-
-
-# mean = np.mean("US_births_2000-2014_SSA.csv"[0:368])
-# print(mean)
-
-# US_births = "US_births_2000-2014_SSA.csv"[0:368]
-# total = np.sum(US_births)
-# print(total)
-
-births_2000 = [
-    "US_births_2000-2014.csv"[0:368]
-]
-for i in "US_births_2000-2014.csv"[0:368]:
-    found = i in "US_births_2000-2014.csv"[0:368]
-    births_2000.append(found)
-print(births_2000)
-
-sum_births_2000 = sum("births_2000")
-print(type(sum_births_2000))
